9004a3f67216cae5ce7de17678d44845b2156147
AL01F13 <fernanda.aguilarj@gmail.com>
2015-06-02 13:57:44 -0500

    CIBCO

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	audio=window.plugins.LawLatencyAudio;
+	audio=window.plugins.LowLatencyAudio;
 	audio.preloadFX('B1','audio/C.mp3',function(){},
 	function(msg){alert("Error "+msg);});
 	audio.preloadFX('B2','audio/D.mp3',function(){},
@@ -10,6 +10,7 @@ document.addEventListener("deviceready",function(){
 	function(msg){alert("Error "+msg);});
 	audio.preloadFX('B4','audio/F.mp3',function(){},
 	function(msg){alert("Error "+msg);});
+	
 	$('#btnjugar').on('tap',function(){
 		var pantalla=$.mobile.getScreenHeight();
 		var encabezado=$('.ui-header').outerHeight();
